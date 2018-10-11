93f9cefc8dc943dce0d5876eb2aa71cea659cab1
Truija99 <truijam@gmail.com>
2018-10-11 12:50:16 +0300

    Lisatud teised arvutustehted, liigutatud tehte loogika meetodisse, lisatud konditsionaalne renderdamine. (Lisatud ka stiili: background, text align:centre)

--- src/components/Konventer.vue
+++ src/components/Konventer.vue
@@ -3,18 +3,18 @@
     <h1>Kaalu teisendamine</h1>
     <h4>Sisesta kaal kilogrammides:</h4>
     <input type="number" min="0" v-model="kg">
-    <table>
-      <tr>
+    <table v-if="kg !== '' " id="output">
+      <tr id="alloutputs">
         <td>Grammid</td>
-        <td>{{ `${kg * 1000} g` }}</td>
+        <td>{{ convertKg(1000) + 'g'}}</td>
       </tr>
       <tr>
         <td>Naelad</td>
-        <td>{{`${(kg * 2.20462262).toFixed(2)} lb`}}</td>
+        <td>{{ convertKg(2.20462262) + 'lb' }}</td>
       </tr>
       <tr>
         <td>Untsid</td>
-        <td>{{ `${(kg * 35.2739619).toFixed(2)} oz`}}</td>
+        <td>{{ convertKg(35.2739619) + 'oz'}}</td>
       </tr>
     </table>
   </div>
@@ -25,16 +25,36 @@ export default {
   name: 'Konventer',
   data () {
     return {
-        kg: 0
+        kg: ''
     }
   },
   methods: {
-     
+     convertKg(multiplier) {
+        /*if (this.kg !== 0) {
+          return (this.kg * multiplier).toFixed(2)
+        } else {
+          return 0
+        }*/
+        return (this.kg !==0) ? (this.kg * multiplier).toFixed (2) : 0
+     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
- 
+    
+    #output {
+      text-align: center;
+      margin-left: auto;
+      margin-right: auto;
+      margin-top: 20px;
+    }
+    input {
+      border: 1px solid black;
+    }
+
+    h1 {
+      margin-top: 50px;
+    }
 </style>
